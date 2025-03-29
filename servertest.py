615d700c86b82c2ae5b6b6a083c9bc453304d8cd
seb <sebastian.christodoulou@rwth-aachen.de>
2025-03-29 19:43:54 +0100

    setup server, then refractor code...

--- servertest.py
+++ servertest.py
@@ -5,25 +5,43 @@ from pydantic import BaseModel
 import roberta
 import tokenizers
 
-# tokenizer = tokenizers.ByteLevelBPETokenizer.from_file('roberta/config/vocab-roberta-base.json', 'roberta/config/merges-roberta-base.txt', lowercase=True, add_prefix_space=True) 
-# roberta.RobertaPredictor(96, 'v0-roberta-0.h5', tokenizer)
-rob = roberta.RobertaPredictor() # "standard" constructor taking default args
 
 app = FastAPI()
 
+# "Standard" model with default args 
+rob = roberta.RobertaPredictor() 
+
 class SentenceItem(BaseModel):
     sentence: str
-    sentiment: str # technically, this could be
+    sentiment: str
 
+class SentenceBatch(BaseModel):
+    sentences: list[str]
+    sentiments: list[str]
+
+
+# receive and process a SentenceItem
 @app.post("/predict_sentence/")
 async def receive_post(item: SentenceItem):
     try:
         sentence_excerpt = rob.predict_sentence(item.sentence, item.sentiment)
         return {"message": "OK", "data": sentence_excerpt}
     except AssertionError:
-        return {"message": "Format Error: Input should be of format '{' sentence: str, sentiment: str '}', where sentiment is \"positive\"/\"neutral\"/\"negative\" ", "data": None}
+        return {"message": "Format Error: Input should be of format '{' sentence: str, sentiment: str '}', where sentiment is 'positive'/'neutral'/'negative'", "data": None}
     except:
         return {"message": "Internal Server Error", "data": None}
+
+# receive and process a SentenceBatch
+@app.post("/predict_sentence_batch/")
+async def receive_post(item: SentenceBatch):
+    try:
+        sentence_excerpts = rob.predict_sentence_batch(item.sentences, item.sentiments)
+        return {"message": "OK", "data": sentence_excerpts}
+    except AssertionError:
+        return {"message": "Format Error: Input should be of format '{' sentences: list[str], sentiments: list[str] '}', where sentiment is 'positive'/'neutral'/'negative', and len(sentiments)==len(sentences)", "data": None}
+    except:
+        return {"message": "Internal Server Error", "data": None}
+
 
 
 if __name__ == "__main__":
