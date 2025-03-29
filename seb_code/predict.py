d096d8a964931a61a3f3c84f2d5bf5baff06fa1b
seb <sebastian.christodoulou@rwth-aachen.de>
2025-03-29 03:56:01 +0100

    we're so back

--- seb_code/predict.py
+++ seb_code/predict.py
@@ -35,37 +35,6 @@ class RobertaPredictor:
 
         return ls, rs
 
-
-    def predict_sentence(self, sentence: str, sentiment: str):
-
-        assert(sentiment in utils.sentiment_id.keys())
-
-        # this merges spaces, and adds a space at the front (if there's none already)
-        sentence_prepared= " "+" ".join(sentence.split())
-        enc = self.tokenizer.encode(sentence_prepared)        
-
-        # TODO: currently 1. Change when chunking
-        input_ids = np.ones((1,self.max_len_tokens),dtype='int32') # actually ones! 1:<pad> in vocab
-        attention_mask = np.zeros((1, self.max_len_tokens), dtype='int32')
-
-        print(utils.sentiment_id[sentiment])
-
-        # fill appropriately
-        input_ids[0,:len(enc.ids)+5] = [0] + enc.ids + [2,2] + [utils.sentiment_id[sentiment]] + [2]
-        attention_mask[0,:len(enc.ids)+5] = 1
-
-        ls, rs = self.predict_tokenized(input_ids, attention_mask)
-
-        l = ls[0]
-        r = rs[0]
-        print("l,r = ", l , r)
-
-        print(enc.ids)
-
-        st = self.tokenizer.decode(input_ids[0, l:r+1])
-
-        return st
-    
     
     def predict_sentence_batch(self, sentences: list[str], sentiments: list[str]):
         # writing is cumbersome, but optimized: allow batchwise prediction inside model!
@@ -93,50 +62,11 @@ class RobertaPredictor:
 
         return sts
     
+
+    def predict_sentence(self, sentence: str, sentiment: str):
+        self.predict_sentence_batch([sentence], [sentiment])
+
+
+
     
-
-
-# TODO: move somewhere else
-import tokenizers
-import utils
-import pandas as pd
-
-MAX_LEN = 96
-
-sentiment_id = {'positive': 1313, 'negative': 2430, 'neutral': 7974}
-
-# make a tokenizer for encodings
-tokenizer = tokenizers.ByteLevelBPETokenizer.from_file('config/vocab-roberta-base.json', 'config/merges-roberta-base.txt', lowercase=True, add_prefix_space=True) # !!!!!!! MATE! THIS SOLVES IT!
-
-test = pd.read_csv('data/test.csv').fillna('')
-train = pd.read_csv('data/train.csv').fillna('')
-input_ids, attention_mask, token_type_ids, start_tokens, end_tokens = utils.prepare_encode_train(train, MAX_LEN, tokenizer)
-input_ids_t, attention_mask_t, token_type_ids_t = utils.prepare_encode_test(test, MAX_LEN, tokenizer)
-
-
-rp = RobertaPredictor(MAX_LEN, '/home/seb/Desktop/CodingChallenge_MLE/v0-roberta-0.h5', tokenizer)
-# rp = RobertaPredictor(MAX_LEN, '/home/seb/Desktop/CodingChallenge_MLE/seb_code/weights_final.h5')
-# TODO: This should be the test. Tokenized predicts same 
-
-ids, ams = input_ids[0:10], attention_mask[0:10]
-ls, rs= rp.predict_tokenized(ids, ams)
-
-# print(ids)
-print(ls, rs)
-predicted_subsequences = [ids[i, l:r+1] for i, (l,r) in enumerate(zip(ls,rs))]
-print(predicted_subsequences)
-sentences = list(map(tokenizer.decode, predicted_subsequences))
-print("\n".join(sentences))
-# tokeni 
-# print(predicted_subsequences)
-
-print("==============================")
-print("==============================")
-
-# print(rp.predict_sentence("my boss is bullying me...","negative"))
-print(rp.predict_sentence_batch(["my boss is bullying me..."],["negative"]))
-
-# print(tokenizer.encode(" the big bad wolf").ids)
-exit(0)
-
-
+    
---
+++ seb_code/tests.py
@@ -0,0 +1,49 @@
+
+from predict import RobertaPredictor
+
+# TODO: move somewhere else
+import tokenizers
+import utils
+import pandas as pd
+
+MAX_LEN = 96
+
+sentiment_id = {'positive': 1313, 'negative': 2430, 'neutral': 7974}
+
+# make a tokenizer for encodings
+tokenizer = tokenizers.ByteLevelBPETokenizer.from_file('config/vocab-roberta-base.json', 'config/merges-roberta-base.txt', lowercase=True, add_prefix_space=True) # !!!!!!! MATE! THIS SOLVES IT!
+
+test = pd.read_csv('data/test.csv').fillna('')
+train = pd.read_csv('data/train.csv').fillna('')
+input_ids, attention_mask, token_type_ids, start_tokens, end_tokens = utils.prepare_encode_train(train, MAX_LEN, tokenizer)
+input_ids_t, attention_mask_t, token_type_ids_t = utils.prepare_encode_test(test, MAX_LEN, tokenizer)
+
+
+rp = RobertaPredictor(MAX_LEN, '/home/seb/Desktop/CodingChallenge_MLE/v0-roberta-0.h5', tokenizer)
+
+
+# This should show us if the predict_sentence_batch works properly
+
+# VERSION 1: tokenize everything manually, predict_tokenized, and then decode
+ids, ams = input_ids[0:10], attention_mask[0:10]
+ls, rs= rp.predict_tokenized(ids, ams)
+print(ls, rs)
+predicted_subsequences = [ids[i, l:r+1] for i, (l,r) in enumerate(zip(ls,rs))]
+print(predicted_subsequences)
+sentences = list(map(tokenizer.decode, predicted_subsequences))
+print("\n".join(sentences))
+
+
+
+print("==============================")
+print("NOTE: Those two versions must be the same")
+print("==============================")
+
+
+# VERSION 2: Above is embedded into a function
+
+
+print("\n".join(rp.predict_sentence_batch(train['text'][0:10],train['sentiment'][0:10])))
+
+
+
